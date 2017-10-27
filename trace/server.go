e9d13123a1fa559af0370d2860feaab718a3cd68
Paul Tyng <ptyng@underarmour.com>
2017-10-27 15:40:08 -0400

    Fix trace to start new span on receive

--- trace/server.go
+++ trace/server.go
@@ -9,19 +9,22 @@ import (
 
 type server struct {
 	inner msg.Server
+	cli   *trace.Client
 }
 
 const receiveSpanName = "go-msg.Server.Receive"
 
 // NewServer returns a go-msg.Server that implements Stackdriver trace.
-func NewServer(inner msg.Server) msg.Server {
+func NewServer(inner msg.Server, cli *trace.Client) msg.Server {
 	return &server{
 		inner: inner,
+		cli:   cli,
 	}
 }
 
 type receiver struct {
 	inner msg.Receiver
+	cli   *trace.Client
 }
 
 func (s *server) Shutdown(ctx context.Context) error {
@@ -31,21 +34,15 @@ func (s *server) Shutdown(ctx context.Context) error {
 func (s *server) Serve(inner msg.Receiver) error {
 	rec := &receiver{
 		inner: inner,
+		cli:   s.cli,
 	}
 
 	return s.inner.Serve(rec)
 }
 
 func (r *receiver) Receive(ctx context.Context, m *msg.Message) error {
-	span := trace.FromContext(ctx)
-	if span != nil {
-		child := span.NewChild(receiveSpanName)
-		defer child.Finish()
-
-		// TODO: labels for attributes? an ID attribute? callback for labels?
-
-		ctx = trace.NewContext(ctx, child)
-	}
-
+	span := r.cli.NewSpan(receiveSpanName)
+	defer span.Finish()
+	ctx = trace.NewContext(ctx, span)
 	return r.inner.Receive(ctx, m)
 }
